9fa78ea428757698d6cdc45dfb6a868fc9f52144
Luqmaan-io <luqmaan-123-@outlook.com>
2025-08-15 00:54:00 +0100

    create flipCard function and click event

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll(".card");
+
+function flipCard(e) {
+    let clickedCard = e.target;
+    clickedCard.classList.add("flip");
+}
+
+// add click event to all cards
+cards.forEach(card => {
+    card.addEventListener("click", flipCard)
+});
